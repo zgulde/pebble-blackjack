2d0305c5b961ca55b8a74baa854616279241c2ab
Zach Gulde <zachgulde@gmail.com>
2016-03-29 21:23:56 -0500

    initial commit of blackjack game

--- src/js/app.js
+++ src/js/app.js
@@ -1,88 +1,44 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var Vector2 = require('vector2');
+var ajax = require('ajax');
+var baseURL = 'http://zgulde.me/cardsapi/decks';
+
+// makes a request to the base url with additional url parameters and data
+// returns the response parsed as JSON
+function request (options) {
+    urlAddition = (typeof options.url    !== 'undefined') ? options.url : '';
+    data        = (typeof options.data   !== 'undefined') ? options.data : {};
+    method      = (typeof options.method !== 'undefined') ? options.method : 'GET';
+
+    return JSON.parse(ajax({
+        url: baseURL + urlAddition,
+        method: method,
+        data: data,
+        async: false
+
+    }).responseText);
+}
+
+function draw (count) {
+    count = (typeof count !== 'undefined') ? count : 1;
+    return request({data: {count: count}}).cards;
+}
+
+
+
+// grab the id of the newly created deck
+var deckid = request({method: 'post', data: {shuffle: true}}).id;
+// all our reqests should now go to that deck id
+baseURL += '/' + deckid;
+
+var dealerHand = draw(2);
+var playerHand = draw(2);
 
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
+  subtitle: dealerHand.map(function(card){ return card.string; }),
   body: 'Press any button.',
   subtitleColor: 'indigo', // Named colors
   bodyColor: '#9a0036' // Hex colors
 });
 
 main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
